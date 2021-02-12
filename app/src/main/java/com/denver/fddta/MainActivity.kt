baffe1e0f87871e759d12e69876d1043c513efdf
Denver Coneybeare <dconeybe@google.com>
2021-02-12 11:38:00 -0500

    MainActivity.kt: add button to launch FirestoreActivity

--- app/src/main/java/com/denver/fddta/MainActivity.kt
+++ app/src/main/java/com/denver/fddta/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.denver.fddta
 
+import com.denver.fddta.firestore.FirestoreActivity
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+  private var btnFirestore: Button? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    btnFirestore = requireViewById(R.id.btn_firestore)
+    btnFirestore!!.setOnClickListener(this)
   }
+
+  override fun onClick(view: View?) {
+    if (view == btnFirestore) {
+      startActivity(Intent(this, FirestoreActivity::class.java))
+    } else {
+      throw IllegalArgumentException("unknown view: $view")
+    }
+  }
+
 }
