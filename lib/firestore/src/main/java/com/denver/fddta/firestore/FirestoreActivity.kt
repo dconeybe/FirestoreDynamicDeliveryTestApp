41328d8813a8ffa48c899919366cd14ff09ae9f0
Denver Coneybeare <dconeybe@google.com>
2021-02-12 10:58:50 -0500

    firestore library skeleton added

---
+++ lib/firestore/src/main/java/com/denver/fddta/firestore/FirestoreActivity.kt
@@ -0,0 +1,11 @@
+package com.denver.fddta.firestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirestoreActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_firestore)
+  }
+}
